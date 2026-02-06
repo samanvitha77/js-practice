1f942b00ef9ace852c5ccf9b43159ec6b0044e70
SAMANVITHA SAJJAN <2320030437@klh.edu.in>
2026-02-06 06:50:11 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+// "use strict"teat all js code as newer version
+// alert(3+3)  we are using nodejs,not browser
+console.log(3
+    + // code readability shld be high
+    3)
+console.log("Pandu")
+let age=18
+let isLoggedIn=false;
+// number=> 2 to power 53
+// bigint
+//string=>"" ''  ``
+//boolean=>true/false
+//null=> standalone value
+//undefined=>value not assigned
+//symbol=>unique identifier
+
+console.log(age)
+console.log(typeof age)
+console.log(typeof null) //bug in js (object)
+console.log(typeof undefined)//object
+
+
