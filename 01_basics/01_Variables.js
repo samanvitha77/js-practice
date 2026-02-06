07e4a72af612b6d24d7137d9ec7c1cd1fb92199b
SAMANVITHA SAJJAN <2320030437@klh.edu.in>
2026-02-06 06:51:18 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,22 @@
+
+const account_Id=20345
+let accountemail="sajjansamanvitha@gmail.com"
+var accountpasscode="12345"
+accountcity="mbnr"
+
+/*{
+// scope
+}*/
+ accountemail="samarshsajjan@gmail.com"
+ var accountpasscode="1968"
+
+// account_Id=2
+/*
+prefer not to use var
+becoz of issue in block scope and functional scope*/
+
+// accountpasscode="34343"
+accountcity="expr"
+console.log(accountpasscode)
+//console.log(accountemail);
+// console.table([account_Id,accountcity,accountpasscode])
